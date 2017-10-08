52a5d3c06c091b14f126df6ecb065c4acedb7320
angelcuevas <angel.p.cuevas@gmail.com>
2017-10-08 18:39:18 -0300

    Starting From Scratch

---
+++ stock/stock/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.clickme = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'clickme
+        '
+        Me.clickme.Location = New System.Drawing.Point(90, 65)
+        Me.clickme.Name = "clickme"
+        Me.clickme.Size = New System.Drawing.Size(75, 23)
+        Me.clickme.TabIndex = 0
+        Me.clickme.Text = "Click me"
+        Me.clickme.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.clickme)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents clickme As Button
+End Class
---
+++ stock/stock/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub clickme_Click(sender As Object, e As EventArgs) Handles clickme.Click
+        MsgBox("You've click me chango")
+    End Sub
+End Class
